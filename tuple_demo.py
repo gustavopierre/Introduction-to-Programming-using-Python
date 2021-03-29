74959379ebf04e47cd34f68f999fadb6019b3b0d
Gustavo Moreira Pierre <gustavopierre@gmail.com>
2021-03-29 22:00:29 +0100

    add examples implemented about lists and tuples

---
+++ tuple_demo.py
@@ -0,0 +1,13 @@
+empty_tuple = tuple()
+print("empty_tuple ->", empty_tuple)
+tuple_str = tuple("hello")
+print("tuple_str ->", tuple_str)
+tuple_list = tuple([1, 2, [3, 5, 7]])
+print("tuple_list ->", tuple_list)
+empty_tuple = ()
+print("empty_tuple ->", empty_tuple)
+singleton_tuple = (1,)
+print("singleton_tuple ->", singleton_tuple)
+tuple_syn = (3, 4, "a", "b")
+print("tuple_syn ->", tuple_syn)
+
